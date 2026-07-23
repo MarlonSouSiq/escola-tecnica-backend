e3ad189afefe5205c06a9642565c9780cea34ad3
Marlon Siqueira <marlonsousiq@gmail.com>
2026-07-23 16:26:37 -0300

    Subindo database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,10 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = (
-    "postgresql+psycopg2://postgres.tbfdgliarpnswugouhfg:NLXA%2Fjm9K%24C%406Ey"
-    "@aws-0-ca-central-1.pooler.supabase.com:5432/postgres"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
